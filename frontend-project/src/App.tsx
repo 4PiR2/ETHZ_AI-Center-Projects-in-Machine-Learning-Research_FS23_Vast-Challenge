58e79b35b8f2c007e235af4365aa9c6e50448f80
Robert Hönig <robhoenig@gmail.com>
2023-05-03 16:05:29 +0200

    Visualize subset of data with Graphin in our React front-end.

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Graphin from '@antv/graphin';
+const data = require('./data/MC1_processed_fraction=0.10.json');
+// const data = require('./data/mock_data.json');
 
-export default App;
+export default () => {
+  return <Graphin data={data} />;
+};
