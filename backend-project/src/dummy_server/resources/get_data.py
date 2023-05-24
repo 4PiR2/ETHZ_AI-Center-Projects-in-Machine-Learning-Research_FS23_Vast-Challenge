111bdef4f5898c1f5b48fc6b8e086897279aebd8
lukas <lukas.petersson.1999@gmail.com>
2023-05-24 11:16:11 +0200

    setters

--- backend-project/src/dummy_server/resources/get_data.py
+++ backend-project/src/dummy_server/resources/get_data.py
@@ -6,15 +6,27 @@ def load_graph(file_path):
     json_data['links'] = json_data.pop('edges')
     return json_data
 
-def load_json(file_path):
-    with open(file_path, 'r') as f:
-        return json.load(f)
-
 def get_graph():
     return load_graph("data/MC1_preprocessed.json")
 
-def get_illegal_nodes():
-    return load_json("data/illegal_ids.json")
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
 
-def get_user_flags():
-    return load_json("data/user_flag_ids.json")
+def write_json(file_path, data):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def add_illegal_node(new_node):
+    illegal_nodes = load_json("data/illegal_ids.json")
+    if new_node not in illegal_nodes:
+        illegal_nodes.append(new_node)
+        write_json("data/illegal_ids.json", illegal_nodes)
+
+def add_user_flag(new_flag):
+    user_flags = load_json("data/user_flag_ids.json")
+    if new_flag not in user_flags:
+        user_flags.append(new_flag)
+        write_json("data/user_flag_ids.json", user_flags)
+
